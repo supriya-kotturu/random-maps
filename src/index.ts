2cb535f4b334430458b261cff8a14c9d0d1bc912
supriya-kotturu <supriyakotturu@pm.me>
2022-01-20 01:39:19 +0530

    add basic defination for User and Company classes

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { User } from './User';
+import { Company } from './Company';
+
+const employee = new User();
+employee.logRandomUser();
+
+const company = new Company();
+console.log(company.companyName);
